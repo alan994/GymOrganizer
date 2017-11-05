0259cbb411ba0da1e26a15dbb0c6e5af27a92637
Alan Jagar <alan.jagar@hotmail.com>
2017-11-05 19:28:53 +0100

    Old client removed

--- Client_Old/src/app/utils/auth-config.ts
+++
@@ -1,8 +0,0 @@
-import { AuthConfig } from 'angular-oauth2-oidc';
-
-export const authConfig: AuthConfig = {
-  issuer: 'http://localhost:5000',
-  redirectUri: window.location.origin + '/home',
-  clientId: 'spa',
-  scope: 'openid profile email',
-};
